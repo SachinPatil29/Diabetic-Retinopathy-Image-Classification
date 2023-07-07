c1a1efb3a9c335ce036f26cb61c50f65cb40b8fe
SachinPatil29 <sachin.cs2002p@gmail.com>
2023-07-08 00:36:45 +0530

    Changed frontend

--- Frontend/src/Users/Home/Home.js
+++ Frontend/src/Users/Home/Home.js
@@ -15,3 +15,73 @@ const Home = () => {
 }
 
 export default Home
+
+
+// import React from 'react';
+// import './Home.css';
+
+
+// const Home = () => {
+//   return (
+//     <>
+//      <center className='title'>
+//         <h2 className='welcome'>Diabteic Retinopathy System</h2>
+        
+//         { <div className='img-main'>
+//           <img className='img3' src="./images/Normal.png"/>
+//           <div className='text1'>
+//           <div className='border1'> 
+//             <h3>Normal</h3>
+//             <p><b>No Potential effects from the diabetes to the eye.</b></p>
+//           </div>
+//           </div>
+//         </div>}
+//         {
+//          <div className='img-main'>
+//          <img className='img4' src="./images/mild.jpg"/>
+//          <div className='text2'>
+//          <div className='border2'>
+//          <h3>Mild NonProliferative Diabteic Retinopathy</h3>
+//             <p><b>This is the earliest stage of diabetic retinopathy, characterized by tiny areas of 
+//                   swelling in the blood vessels of the retina. These areas of swelling are known as micro aneurysms.
+//                   Small amounts of fluid can leak into the retina at the stage, triggering swelling of the macula. 
+//                   This is an area near the center of the retina.</b></p>
+//           </div>
+//           </div>
+//        </div>}
+//        {
+//          <div className='img-main'>
+//          <img className='img5' src="./images/moderate.jpg"/>
+//          <div className='text3'>
+//          <div className='border3'>
+//          <h3>Moderate NonProliferative Diabteic Retinopathy</h3>
+//             <p><b>Increased swelling of tiny blood vessels starts to interfere with blood flow to the retina, preventing proper nourishment. This causes an accumulation of blood and other fluids in the macula.</b></p>
+//           </div>
+//           </div>
+//        </div>}
+//        {
+//          <div className='img-main'>
+//          <img className='img6' src="./images/severe.jpg"/>
+//          <div className='text4'>
+//           <div className='border4'>
+//          <h3>Severe NonProliferative Diabteic Retinopathy</h3>
+//             <p><b>A larger section of blood vessels in the retina become blocked, causing a significant decrease in blood flow to this area. At this point, the body receives signals to start growing new blood vessels in the retina.</b></p>
+//           </div>
+//           </div>
+//        </div>}
+//        {
+//          <div className='img-main'>
+//          <img className='img7' src="./images/proliferative.jpg"/>
+//          <div className='text5'>
+//          <div className='border5'>
+//          <h3>Proliferative Diabteic Retinopathy</h3>
+//             <p><b>This is an advanced stage of the disease, in which new blood vessels form in the retina. Since these blood vessels are often fragile, there’s a higher risk of fluid leakage. This triggers different vision problems such as blurriness, reduced field of vision, and even blindness.</b></p>
+//           </div>
+//           </div>
+//        </div>}
+//      </center>
+//     </>
+//   )
+// }
+
+// export default Home
